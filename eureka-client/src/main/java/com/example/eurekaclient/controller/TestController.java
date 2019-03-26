dd09ddbab7a8a4eb48848dd4572fd9889ebd7528
zhaoyibin <zhaoyibin@yuewen.com>
2019-03-26 19:17:18 +0800

    client

---
+++ eureka-client/src/main/java/com/example/eurekaclient/controller/TestController.java
@@ -0,0 +1,21 @@
+package com.example.eurekaclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by zhaoyibin on 19:04 2019/3/26
+ */
+@RestController
+public class TestController {
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hi")
+    public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
+        return "hi " + name + " ,i am from port:" + port;
+    }
+}
