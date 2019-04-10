30e9584b58e0ad1f5cbae69b454b9589fcedeee5
zhaoyibin <zhaoyibin@yuewen.com>
2019-04-10 16:21:55 +0800

    add config-client

---
+++ config-client/src/main/java/com/example/configclient/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.example.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by zhaoyibin on 15:59 2019/4/10
+ */
+@RestController
+public class TestController {
+    @Value("${foo}")
+    String foo;
+
+    @RequestMapping(value = "/hi")
+    public String hi() {
+        return foo;
+    }
+}
